c44399bd466fb881a0d77a780fc7f8426abc36f6
zhoupeng <zhoupeng@xjgreat.com>
2018-09-19 18:05:41 +0800

    just works

--- src/luna.nim
+++ src/luna.nim
@@ -23,7 +23,6 @@ const
     preserveCommands = ["equals","matchs","less","greater"]
     indentSpaceNum = 4
 
-# proc onRaiseAction(e: ref Exception):bool =
 proc getIndentLevel(call:string,col:Natural):untyped {.compileTime.}=
     result = (col - call.len) div indentSpaceNum
 
@@ -37,33 +36,77 @@ template describe(des:string,body:untyped):untyped =
     block:
         body
 
-proc fails(n:NimNode,indent:Natural):NimNode{.compileTime.} =
-    let flag = newStrLitNode("✘  ".indent( indent * indentSpaceNum ))
+proc fails(n:NimNode):NimNode{.compileTime.} =
+    let indentLevel = getIndentLevel(n.toStrLit.strVal,lineInfoObj(n).column) - 1
+    let flag = newStrLitNode("✘  ".indent( indentLevel * indentSpaceNum ))
     result = newCall(newIdentNode"styledWriteLine",newIdentNode("stdout"),newIdentNode("fgRed"),flag,newIdentNode("resetStyle"),n.toStrLit)
 
-proc pass(n:NimNode,indent:Natural):NimNode{.compiletime.} =
-    let flag = newStrLitNode("✓  ".indent(indent * indentSpaceNum))
+proc pass(n:NimNode):NimNode{.compileTime.} =
+    let indentLevel = getIndentLevel(n.toStrLit.strVal,lineInfoObj(n).column) - 1
+    let flag = newStrLitNode("✓  ".indent(indentLevel * indentSpaceNum))
     result = newCall(newIdentNode"styledWriteLine",newIdentNode("stdout"),newIdentNode("styleDim"),flag,newIdentNode("resetStyle"),n.toStrLit)
 
-macro expect(n:untyped): untyped =
-    # var outputStream = newFileStream(stdout)
+proc ifelse(n:NimNode):NimNode{.compileTime.} =
     result = newNimNode(nnkStmtList,n)
-    let indentLevl = getIndentLevel(n.toStrLit.strVal,lineInfoObj(n).column) - 1
+    
+    let indentLevel = getIndentLevel(n.toStrLit.strVal,lineInfoObj(n).column) - 1
+    result.add newIfStmt(
+        (n, pass(n)),
+      ).add(newNimNode(nnkElse).add(fails(n)))
+
+proc exceptionHandle(n:NimNode): NimNode =
+    var indentLevel:int
+    indentLevel = max(getIndentLevel(n.toStrLit.strVal,lineInfoObj(n).column) - 1,1)
+    result = quote do:
+        var msg = ""
+        let ex = getCurrentException()
+        let stacks = getStackTraceEntries(ex)
+        let last = stacks[^1]
+        msg.add last.procname
+        msg.add " [$#]" % $ex.name
+        styledWriteLine(stdout,fgRed,"✘  ".indent(`indentLevel` * indentSpaceNum ),resetStyle,msg )
+
+macro expect(n:untyped): untyped =
+    let 
+        childs = toSeq(n.children)
+        childsLen = len(childs)
+        first = childs[0]
+        firstChilds = toSeq(first.children)
+
+    let
+        second = childs[1]
+        secondChilds = toSeq(second.children)
+
+    case second.kind:
+        of nnkCommand,nnkCall:
+            if secondChilds[0].basename.strVal == "takes":
+                result = newNimNode(nnkTryStmt,n)
+                var call = newNimNode(nnkCall)
+                call.add first
+                for x in secondChilds[1..^1]:
+                    call.add x
+                result.add call
+                result.add newNimNode(nnkExceptBranch).add(exceptionHandle(n))
+            else:
+                result = ifelse(n)
+        else:
+            result = ifelse(n)
+
+    # var outputStream = newFileStream(stdout)
+    # result = newNimNode(nnkStmtList,n)
+    # let indentLevl = getIndentLevel(n.toStrLit.strVal,lineInfoObj(n).column) - 1
     # let command = newNimNode(nnkCommand,n)
     # command.add newIdentNode("doAssert")
     # command.add n
     # result.add command
-    result.add newIfStmt(
-        (n, pass(n,indentLevl)),
-      ).add(newNimNode(nnkElse).add(fails(n,indentLevl)))
     
     # if output.isatty():
     # outputStream.write()
-    
 
 when isMainModule:
     proc plus(a,b:int):int = a + b
-            
+    proc newEx(n:int) = raise newException(ValueError,"")   
+    proc newExt(n,b:int) = raise newException(ValueError,"")      
     describe "test call":
         # parallel:
         #     spawn expect plus(1,2) == 3
@@ -73,3 +116,7 @@ when isMainModule:
 
         describe "test nested":
             expect plus(1,2) == 3
+
+    describe "test takes and exception":
+        expect newEx takes 1
+        expect newExt takes(1,2)
